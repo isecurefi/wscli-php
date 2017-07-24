b80e4514ee569cca4422c3555f089d6ff9cc410d
Dan Forsberg <dforsber@gmail.com>
2017-07-24 13:58:22 +0300

    update bump version

--- wscli-php/bump-version.sh
+++ wscli-php/bump-version.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ $# -ne 1 ]; then
   echo "Usage: `basename $0` <tag>"
-  exit 65
+  exit 1
 fi
 
 TAG=$1
@@ -13,31 +13,25 @@ TAG=$1
 # Tag & build master branch
 #
 git checkout master
-git tag -a -m "v${TAG}" ${TAG}
+git tag -a -m "**Release v${TAG}**" ${TAG}
 make release
 
 #
 # Copy executable file into GH pages
 #
 
-(
-    git checkout gh-pages
-    git add wscli.phar
-)
+cp wscli.phar ..
+cp wscli.phar.pubkey ..
 
 SHA1=$(openssl sha1 wscli.phar | cut -f 2 -d ' ')
-if [ -f wscli.phar.pubkey ];
-then
-    (
-        git checkout gh-pages
-        git add wscli.phar.pubkey
-    )
-fi
 (
+    cd ..
     git checkout gh-pages
     echo $SHA1 > wscli.phar.version
+    git add wcsli.phar
+    git add wscli.phar.pubkey
     git add wscli.phar.version
-    git commit -m "Version $TAG with sha1 $SHA1" wscli.phar.version wscli.phar
+    git commit -m "Version $TAG with sha1 $SHA1" wscli.phar.version wscli.phar wscli.phar.pubkey
     git push
 )
 
